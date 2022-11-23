0c57b7a2dd5a192309eda2ca9aec598ae5855926
luojinbo <luojinbo@enn.cn>
2022-11-24 01:34:13 +0800

    - encode - decode - store

--- src/git_op.rs
+++ src/git_op.rs
@@ -1,6 +1,6 @@
-use std::{fmt::format, env, path::Path, fs::File, io::Write, f64::consts::E};
+use std::{fmt::format, env, path::Path, fs::{File, self}, io::{Write, Read}, f64::consts::E};
 
-use crate::utils::{zlib_encode, sha1_encode};
+use crate::utils::{zlib_encode, sha1_encode, zlib_decode};
 
 enum Command {
     Add,
@@ -24,13 +24,16 @@ impl GitBaseObj {
         let final_ctx = zlib_encode(&git_obj_ctx);
         let hash_code = sha1_encode(&self.content);
         // store
-        let file_path_str = format!("./objects/{}/{}", &hash_code[..2], &hash_code[2..]);
+        let root_path = env::current_dir().expect("current_dir env path fail.");
+        let curr_dir = root_path.to_str().expect("env path fail.");
+        let file_path_str = format!("{}\\objects\\{}\\{}", curr_dir, &hash_code[..2], &hash_code[2..]);
         let file_path = Path::new(&file_path_str);
         match file_path.exists() {
             true => print!("{file_path_str} is exists!"),
             false => {
-                let parent_dir = file_path.parent().unwrap();
-                File::create(&parent_dir).unwrap();
+                let parent_dir = file_path.parent().expect("Executable must be in some directory");
+                println!("{:?}", parent_dir);
+                fs::create_dir_all(&parent_dir).unwrap();
             },
         }
         let mut file = File::create(&file_path).unwrap();
@@ -41,6 +44,14 @@ impl GitBaseObj {
     }
 }
 
+fn cat_file(file_path: &str) -> String {
+    let path = Path::new(file_path);
+    let mut f = File::open(path).unwrap();
+    let mut content:Vec<u8> = Vec::new();
+    let _ = f.read_to_end(&mut content);
+
+    zlib_decode(&content)
+}
 
 #[cfg(test)]
 
@@ -48,6 +59,11 @@ mod tests {
     use super::*;
     #[test]
     fn test_git_rust() {
-
+        let s = String::from("version1").into_bytes();
+        let test001 = GitBaseObj::new("blob".to_string(), s, "test001".to_string());
+        let s_rst = test001.store_self();
+        println!("{:?}", s_rst);
+        let src_content = cat_file(&s_rst.unwrap());
+        println!("{:?}", src_content);
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,15 +1,30 @@
-pub fn zlib_encode(content: &str) -> Vec<u8> {
 
-    todo!()
+use std::io::Read;
+
+use flate2::{bufread::{ZlibEncoder, ZlibDecoder}, Compression};
+use hex::ToHex;
+use sha1::{Sha1, Digest};
+
+pub fn zlib_encode(content: &str) -> Vec<u8> {
+    let mut encoder = ZlibEncoder::new(content.as_bytes(), Compression::fast());
+    let mut buffer = Vec::new();
+    let _ = encoder.read_to_end(&mut buffer);
+    buffer
 }
 
 pub fn zlib_decode(content: &Vec<u8>) -> String {
+    let mut decoder = ZlibDecoder::new(&content[..]);
+    let mut rst = String::new();
+    let _ = decoder.read_to_string(&mut rst);
 
-    todo!()
+    rst
 }
 
 pub fn sha1_encode(content: &Vec<u8>) -> String {
-    todo!()
+    let mut encoder = Sha1::new();
+    encoder.update(String::from_utf8(content.clone()).unwrap());
+    let e_rst: Vec<u8> = encoder.finalize()[..].to_vec();
+    e_rst.encode_hex()
 }
 
 
